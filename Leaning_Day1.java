5f9828e639f20bbf51931d58c4cd2a189a16fe29
kb.carlos <164322634+MakeMePBaJ@users.noreply.github.com>
2025-02-04 22:23:37 +0100

    Update Leaning_Day1.java
    updated code

--- Leaning_Day1.java
+++ Leaning_Day1.java
@@ -4,24 +4,26 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        // input
-        String kontoSparkasseName = "Sparkasse ";
-        String kontoN26Name = "N26 ";
+        // Input: Define the names of the bank accounts
+        String bankAccountSparkasseName = "Sparkasse "; // Sparkasse bank account
+        String bankAccountN26Name = "N26 "; // N26 bank account
 
-        int kontoSparkasseGeld = 2900;
-        int kontoN26Geld = 99;
+        // Input: Define the amount of money in each bank account
+        int bankAccountSparkasseMoney = 2900; // Money in Sparkasse account
+        int bankAccountN26Money = 99; // Money in N26 account
 
-        int alleKonten = kontoSparkasseGeld + kontoN26Geld;
+        // Calculate the total money in all accounts
+        int totalMoney = bankAccountSparkasseMoney + bankAccountN26Money;
 
-        //output
+        // Output: Print the amount of money in each account and the total
         System.out.println(
-            "Auf seinem " + kontoSparkasseName + "Konto sind " + kontoSparkasseGeld + "€."
+            "There are " + bankAccountSparkasseMoney + "€ in the " + bankAccountSparkasseName + "account."
         );
         System.out.println(
-            "Auf seinem " + kontoN26Name + "Konto sind " + kontoN26Geld + "€."
+            "There are " + bankAccountN26Money + "€ in the " + bankAccountN26Name + "account."
         );
         System.out.println(
-            "Auf beiden Konten sind " + alleKonten + "€."
+            "There are " + totalMoney + "€ in both accounts."
         );
 
     }
